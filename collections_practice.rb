447abb266b7b3cc8d2d50eb9e378a493ba7aff56
Trent Moss <trentmoss05@gmail.com>
2019-01-16 20:42:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse!
+end
+
+def sort_array_char_count(array)
+  array.sort { |x,y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  kesha_array = []
+  array.each do |string|
+    kesha_array << string[2]
+end
