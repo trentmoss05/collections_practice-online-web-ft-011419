5fdcc66b8c995afdd973c8521b3ddb8df52754a8
Trent Moss <trentmoss05@gmail.com>
2019-01-16 20:56:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,7 +37,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |element|
+  array.each_with_index.collect do |element, index|
   if array[1] = element
     element
   else
